50c85d9baac7e5da56f0fb1cbc951d40bfe77d6d
lucio-souza <117987746+lucio-souza@users.noreply.github.com>
2023-09-18 01:33:52 -0300

    Update main.java

--- exercicio04_CT/main.java
+++ exercicio04_CT/main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in)
+		Scanner sc = new Scanner(System.in);
 		int num;
 		float valor;
 		conta Conta=new conta();
