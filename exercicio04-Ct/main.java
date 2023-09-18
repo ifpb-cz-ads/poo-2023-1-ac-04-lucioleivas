d026c4ed3514ca0b367654e9d0c99bf151b4638b
lucio-souza <117987746+lucio-souza@users.noreply.github.com>
2023-09-18 01:31:46 -0300

    Update main.java

--- exercicio04-Ct/main.java
+++ exercicio04-Ct/main.java
@@ -1,33 +1,31 @@
-package exercicio04-CT;
+package exercicio04_CT;
 import java.util.Scanner;
 
 public class main {
 	public static void main(String[] args) {
-		try (Scanner sc = new Scanner(System.in)) {
-			int num;
-			float valor;
-			conta Conta=new conta();
-			do {
-				System.out.println("---MENU---");
-				System.out.println("digite 1 para depositar");
-				System.out.println("digite 2 para sacar");
-				System.out.println("digite 0 para sair");
-				num=sc.nextInt();
-				if(num==1){
-					System.out.println("informe o valor para depositar");
-					valor=sc.nextFloat();
-					Conta.depositar(valor);
-					System.out.println(Conta.saldo);
+		Scanner sc = new Scanner(System.in)
+		int num;
+		float valor;
+		conta Conta=new conta();
+		do {
+			System.out.println("---MENU---");
+			System.out.println("digite 1 para depositar");
+			System.out.println("digite 2 para sacar");
+			System.out.println("digite 0 para sair");
+			num=sc.nextInt();
+			if(num==1){
+				System.out.println("informe o valor para depositar");
+				valor=sc.nextFloat();
+				Conta.depositar(valor);
+			}
+			if(num==2) {
+				System.out.println("informe o valor para sacar");
+				valor=sc.nextFloat();
+				if(Conta.sacar(valor)) {
+					System.out.println("valor sacado com sucesso");
+				}else {
+					System.out.println("saldo insuficiente");
 				}
-				if(num==2) {
-					System.out.println("informe o valor para sacar");
-					valor=sc.nextFloat();
-					if(Conta.sacar(valor)) {
-						System.out.println("valor sacado com sucesso");
-					}else {
-						System.out.println("saldo insuficiente");
-					}
-					System.out.println(Conta.saldo);
 				}
 			}while(num!=0);
 		}
